70780f6047fdea332e1b68ea2a1e76284bf54cde
Amgalanbaatar1 <amgaabra08@gmail.com>
2024-01-17 20:17:01 +0800

    portfolio day2

--- componendss/icons/Menuitem.jsx
+++ componendss/icons/Menuitem.jsx
@@ -1,3 +1,18 @@
-export function Menuitems() {
-  return;
+export function Menuitems({ items }) {
+  return (
+    <div className="hidden md:flex items-center justify-end flex-1">
+      <a className="px-2 py4" href="{items.[0].link}">
+        {items[0].label}
+      </a>
+      <a className="px-2 py4 " href="{items.[0].link}">
+        {items[1].label}
+      </a>
+      <a className="px-2 py4 " href="{items.[0].link}">
+        {items[2].label}
+      </a>
+      <a className="px-2 py4 " href="{items.[0].link}">
+        {items[3].label}
+      </a>
+    </div>
+  );
 }
--- pages/Header.js
+++ pages/Header.js
@@ -11,11 +11,12 @@ export function Header() {
   return (
     <div className="border-2 flex justify-between p-4  container mx-auto">
       <Logo />
-      <div className="sm:flex md:hidden">
+      <div className="flex md:hidden">
         <Menu />
       </div>
-      <Menuitems />
+
       <div className="hidden md:flex list-none gap-5">
+        <Menuitems items={menuItems} />
         <img className="w-9" src="/images/icon.png" alt="ModeIcon" />
         <div className="border bg-black  flex rounded-xl w-32 min-h-9 justify-center ali text-white items-center">Download CV</div>
       </div>
